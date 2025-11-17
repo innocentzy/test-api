a5337fcb2c48bcc299d357392b2488209cf74a18
innocentzy <nikitka.kat@mail.ru>
2025-11-17 18:31:09 +0300

    Add docker-compose

--- app/database.py
+++ app/database.py
@@ -6,7 +6,7 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-DATABASE_URL = os.getenv("DB_URL")
+DATABASE_URL = os.getenv("DATABASE_URL")
 
 engine = create_engine(DATABASE_URL)                                                # Подключение к базе данных
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)         # Создание сессии для работы с БД
